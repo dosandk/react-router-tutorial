6957bafdffbd3be482d2905ec9a6af2d655e8e45
Vladimir Shevchuk <dosandkv@gmail.com>
2016-05-12 10:53:40 +0300

    04-lesson;

--- lessons/04-nested-routes/modules/App.js
+++ lessons/04-nested-routes/modules/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>React Router Tutorial</h1>
-        <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
-        </ul>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Ghettohub Issues</h1>
+                <ul role="nav">
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/repos">Repos</Link></li>
+                </ul>
+
+                { this.props.children }
+            </div>
+        )
+    }
 })
